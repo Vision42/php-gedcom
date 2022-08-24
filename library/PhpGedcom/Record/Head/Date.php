1e11f0b33a0239c8ef782b42477da14ea9c0f0b6
Philipp Limmer <philipp.limmer@web.de>
2022-08-24 22:31:41 +0200

    [TASK] Add getter / setter to Date-Class

--- library/PhpGedcom/Record/Head/Date.php
+++ library/PhpGedcom/Record/Head/Date.php
@@ -22,4 +22,37 @@ class Date extends \PhpGedcom\Record
 {
     protected $_date = null;
     protected $_time = null;
+
+    /**
+     * @return null
+     */
+    public function getDate()
+    {
+        return $this->_date;
+    }
+
+    /**
+     * @param null $date
+     */
+    public function setDate($date): void
+    {
+        $this->_date = $date;
+    }
+
+    /**
+     * @return null
+     */
+    public function getTime()
+    {
+        return $this->_time;
+    }
+
+    /**
+     * @param null $time
+     */
+    public function setTime($time): void
+    {
+        $this->_time = $time;
+    }
+
 }
